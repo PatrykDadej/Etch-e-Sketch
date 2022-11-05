a1e81dfdcf10b75e4e23455fd729ee406fee6fa3
Patryk <patrykdadej@gmail.com>
2022-11-05 23:01:48 +0000

    Optimise code, add eraser button

--- Sketchjs.js
+++ Sketchjs.js
@@ -1,5 +1,8 @@
 let desiredSize
 
+const boardSelector = document.querySelector(".board")
+const palette = ["red","blue","purple","white","yellow","green","orange","black","grey"]
+
 function sizeRequester () {
     desiredSize = prompt("Please choose size of the board/nAllowed length of div size is between 16-64")
     for ( ; desiredSize < 16 || desiredSize > 64 ; ) {
@@ -7,11 +10,70 @@ function sizeRequester () {
     }
 }
 
+function clear(){
+    for (let p = 1 ; p <= Math.pow(desiredSize,2) ; p++){
+            this["square"+p].style.backgroundColor = ""
+    }
+}
+
+function rainbower (){
+    for (let p = 1 ; p <= Math.pow(desiredSize,2) ; p++){
+        this["square"+p].addEventListener("mouseover", () => {
+            currentColor = palette[Math.floor(Math.random()*palette.length)]
+            this["square"+p].style.backgroundColor = currentColor
+        })
+        this["square"+p].addEventListener("mouseout", () => {
+            this["square"+p].style.backgroundColor = currentColor
+        })
+    
+    }
+}
+
+function normaliser (){
+    for (let p = 1 ; p<=Math.pow(desiredSize,2) ; p++){
+        this["square"+p].addEventListener("mouseover", () => {
+            this["square"+p].style.backgroundColor = "grey";
+        })
+        this["square"+p].addEventListener("mouseout", () => {
+            this["square"+p].style.backgroundColor = "grey"
+        
+        })
+        
+    }
+    
+}
+
+function customColor() {
+    for (let p = 1 ; p<=Math.pow(desiredSize,2) ; p++){
+        this["square"+p].addEventListener("mouseover", () => {
+            this["square"+p].style.backgroundColor = pickerColor;
+        })
+        this["square"+p].addEventListener("mouseout", () => {
+            this["square"+p].style.backgroundColor = pickerColor
+        })
+        
+    }
+    
+}
+
+function eraser (){
+    for (let p = 1 ; p<=Math.pow(desiredSize,2) ; p++){
+        this["square"+p].addEventListener("mouseover", () => {
+            this["square"+p].style.backgroundColor = "#d4d4d4";
+        })
+        this["square"+p].addEventListener("mouseout", () => {
+            this["square"+p].style.backgroundColor = "#d4d4d4"
+        
+        })
+        
+    }
+    
+}
+
 sizeRequester()
 
 let squareSize = 500/desiredSize
-
-const boardSelector = document.querySelector(".board")
+let currentColor
 let count = 1
 
 for (let i = 1 ; i <=desiredSize ; i++) {
@@ -40,74 +102,27 @@ for (let p = 1 ; p<=Math.pow(desiredSize,2) ; p++){
     })
 
 }
-const palette = ["red","blue","purple","white","yellow","green","orange","black","grey"]
-function eraser(){
-    for (let p = 1 ; p<=Math.pow(desiredSize,2) ; p++){
-            this["square"+p].style.backgroundColor = ""
 
-    }
-}
-
-const eraserSelector = document.querySelector('.eraser')
-eraserSelector.addEventListener("click",() => eraser())
-
-let currentColor
-
-function rainbower (){
-    for (let p = 1 ; p<=Math.pow(desiredSize,2) ; p++){
-        this["square"+p].addEventListener("mouseover", () => {
-            currentColor = palette[Math.floor(Math.random()*palette.length)]
-            this["square"+p].style.backgroundColor = currentColor
-        })
-        this["square"+p].addEventListener("mouseout", () => {
-            this["square"+p].style.backgroundColor = currentColor
-        })
-    
-}}
+const clearSelector = document.querySelector('.clear')
+clearSelector.addEventListener("click",() => clear())
 
 const normaliserSelector = document.querySelector('.normaliser')
 normaliserSelector.addEventListener('click',() => normaliser()
 )
 
-function normaliser (){
-    for (let p = 1 ; p<=Math.pow(desiredSize,2) ; p++){
-        this["square"+p].addEventListener("mouseover", () => {
-            this["square"+p].style.backgroundColor = "grey";
-        })
-        this["square"+p].addEventListener("mouseout", () => {
-            this["square"+p].style.backgroundColor = "grey"
-        
-            })
-        
-    }
-    
-}
-
 const rainbowerSelector = document.querySelector('.rainbower')
-rainbowerSelector.addEventListener('click', () => {
-    rainbower()
-})
+rainbowerSelector.addEventListener('click', () => rainbower())
+
+const eraserSelector = document.querySelector('.eraser')
+eraserSelector.addEventListener('click', () => eraser())
 
 const colorPickerSelector = document.querySelector('#colorPicker')
 
-let kolorek
+let pickerColor
 
 colorPickerSelector.onchange = function () {
-    kolorek = colorPickerSelector.value
+    pickerColor = colorPickerSelector.value
     customColor()
 }
 
-function customColor() {
-    for (let p = 1 ; p<=Math.pow(desiredSize,2) ; p++){
-        this["square"+p].addEventListener("mouseover", () => {
-            this["square"+p].style.backgroundColor = kolorek;
-        })
-        this["square"+p].addEventListener("mouseout", () => {
-            this["square"+p].style.backgroundColor = kolorek
-        
-            })
-        
-    }
-    
-}
 
